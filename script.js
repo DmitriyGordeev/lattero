6fa660f5b351ff9085d680a599b40a28178060a4
Weekend Master <weekend@super.com>
2017-11-02 16:38:16 +0300

    tabs are work, but look ugly

--- script.js
+++ script.js
@@ -24,12 +24,72 @@ function material_setup() {
         mdc.textfield.MDCTextfield.attachTo(mdcTextFields[i]);
     }
 
+
 }
 
+function setup_tab() {
+
+    var dynamicTabBar = window.dynamicTabBar = new mdc.tabs.MDCTabBar(document.querySelector('#dynamic-tab-bar'));
+    var dots = document.querySelector('.dots');
+    var panels = document.querySelector('.panels');
+
+    dynamicTabBar.preventDefaultOnClick = true;
+
+    function updateDot(index) {
+        var activeDot = dots.querySelector('.dot.active');
+        if (activeDot) {
+            activeDot.classList.remove('active');
+        }
+        var newActiveDot = dots.querySelector('.dot:nth-child(' + (index + 1) + ')');
+        if (newActiveDot) {
+            newActiveDot.classList.add('active');
+        }
+    }
+
+    function updatePanel(index) {
+        var activePanel = panels.querySelector('.panel.active');
+        if (activePanel) {
+            activePanel.classList.remove('active');
+        }
+        var newActivePanel = panels.querySelector('.panel:nth-child(' + (index + 1) + ')');
+        if (newActivePanel) {
+            newActivePanel.classList.add('active');
+        }
+    }
+
+    dynamicTabBar.listen('MDCTabBar:change', function (t) {
+        var dynamicTabBar = t.detail;
+        var nthChildIndex = dynamicTabBar.activeTabIndex;
+
+        updatePanel(nthChildIndex);
+        updateDot(nthChildIndex);
+    });
+
+    dots.addEventListener('click', function (evt) {
+        if (!evt.target.classList.contains('dot')) {
+            return;
+        }
+
+        evt.preventDefault();
+
+        var dotIndex = [].slice.call(dots.querySelectorAll('.dot')).indexOf(evt.target);
+
+        if (dotIndex >= 0) {
+            dynamicTabBar.activeTabIndex = dotIndex;
+        }
+
+        updatePanel(dotIndex);
+        updateDot(dotIndex);
+    });
+}
 
 jQuery(document).ready(function() {
 
     material_setup();
+    setup_tab();
+
+
+
     jQuery(".question-content-expanded").toggle();
     jQuery("#dialog-question-fabs").toggle();
 
